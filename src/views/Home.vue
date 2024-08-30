2be2c1e6a08fa3bd27ba25de025b92e5124df9ed
tan-sd <shengdatan@gmail.com>
2024-08-30 15:04:02 +0800

    Edit styling and Add Login and Home page

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <main>
+        <div class="welcome-container">
+            <div class="profile-picture"></div>
+            <div class="welcome-message">Hi, John Doe!</div>
+        </div>
+    </main>
+</template>
+
+<style scoped>
+.welcome-container {
+    color: black;
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+}
+
+.profile-picture {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    border: 1px solid black;
+}
+
+.welcome-message {
+    margin-left: 10px;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="login-container">
+        <h2>Login</h2>
+        <form>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input
+                    type="email"
+                    id="email"
+                    v-model="email"
+                    required
+                    placeholder="Enter your email"
+                />
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    v-model="password"
+                    required
+                    placeholder="Enter your password"
+                />
+            </div>
+            <!-- <button type="submit">Login</button> -->
+            <button>
+                <RouterLink to="/home">Login</RouterLink>
+            </button>
+            <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
+        </form>
+    </div>
+</template>
+
+<style scoped>
+.login-container {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 300px;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    background-color: #fafafa;
+    color: black;
+}
+
+h2 {
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+.form-group {
+    margin-bottom: 15px;
+}
+
+label {
+    display: block;
+    margin-bottom: 5px;
+}
+
+input[type="email"],
+input[type="password"] {
+    width: 100%;
+    padding: 8px;
+    box-sizing: border-box;
+    border-radius: 4px;
+    border: 1px solid #ccc;
+}
+
+button {
+    width: 100%;
+    padding: 10px;
+    background-color: #fb923c;
+    color: white;
+    font-weight: bold;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+button:hover {
+    background-color: #ff7300;
+}
+
+.error-message {
+    color: red;
+    margin-top: 10px;
+    text-align: center;
+}
+</style>
