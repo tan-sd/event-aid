8c77be6b90a85da2534f6b9278f19f3dd0e90906
ryanteojw <ryanteo_99@hotmail.com>
2024-08-31 00:09:01 +0800

    added db models

---
+++ backend/models/user_event.py
@@ -0,0 +1,9 @@
+from sqlalchemy import ForeignKey
+from __init__ import db
+
+class User_event(db.Model):
+    __tablename__ = 'User_event'
+    event_id = db.Column(db.Integer, ForeignKey('event.event_id'), primary_key=True)
+    senior_username = db.Column(db.String(100), ForeignKey('Senior.username'), primary_key=True)
+    is_sign_up = db.Column(db.Boolean)
+    is_confirmed = db.Column(db.Boolean)
