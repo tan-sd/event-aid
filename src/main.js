deaa2759235940af01378f9481fc2a0e48c88f50
Adam Bin Firdaus Tan <adambft98@gmail.com>
2024-08-31 10:39:01 +0800

    added fontawesome library

--- src/main.js
+++ src/main.js
@@ -6,8 +6,21 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faPlus)
+
 const app = createApp(App);
 
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router);
 
 app.mount("#app");
